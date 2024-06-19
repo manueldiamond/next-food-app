708990dc47d09201a0644134f9e550a0b36225ff
manueldiamond <manueldiamondlistowell@gmail.com>
2024-06-19 02:19:21 +0000

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,43 @@ const lobster = Lobster({
 export const metadata: Metadata = {
   title: "Tasty Chef",
   description: "Order food around Central Campus online and have it delivered right to your doorstep!",
+  applicationName: "Tasty Chef",
+  authors: [{ name: "Manuel D.", url: "https://github.com/manueldiamond/" },{name:'TABI'},{name:'Lyrics Jnr'}],
+  generator: "Next.js",
+  keywords: ["food delivery", "online ordering", "Central Campus", "Tasty Chef"],
+  referrer: "origin-when-cross-origin",
+  themeColor: "#EF2A39",
+  colorScheme: "light",
+  creator: "Manuel D. ,TABI, LyricsJnr",
+  publisher: "Tasty Chef Inc.",
+
+  manifest: "https://yourwebsite.com/manifest.json",
+  openGraph: {
+    type: "website",
+    url: "https://yourwebsite.com",
+    title: "Tasty Chef",
+    description: "Order food around Central Campus online and have it delivered right to your doorstep!",
+    siteName: "Tasty Chef",
+    images: [{
+      url: "https://i.ibb.co/PxFb4S8/image-2024-06-19-020449669.png",
+      
+      alt: "Tasty Chef"
+    }]
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@TastyChef",
+    title: "Tasty Chef",
+    description: "Order food around Central Campus online and have it delivered right to your doorstep!",
+    images: ["https://i.ibb.co/PxFb4S8/image-2024-06-19-020449669.png"]
+  },
+
+  
+  category: "Food & Drink, Delivery App",
+  classification: "Online Food Delivery",
 };
+
+
 
 export default function RootLayout({
   children,
